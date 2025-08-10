cdbd81d2b56566ffe8a2b7143c325eb61b6f8706
VenkatDugasani <132843601+VenkatDugasani@users.noreply.github.com>
2025-08-10 14:25:23 -0700

    Time: 0 ms (100%), Space: 42.8 MB (46.18%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    List<List<Integer>> result = new ArrayList<>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+        helper(nums, 0, new ArrayList<>());
+        return result;
+        
+    }
+    public void helper(int[] nums, int i, List<Integer> slate){
+        if(i == nums.length){
+            result.add(new ArrayList<>(slate));
+            return;
+        }
+
+        helper(nums, i + 1, slate); //Exclude case
+
+        slate.add(nums[i]);
+        helper(nums, i + 1, slate);
+        slate.remove(slate.size() - 1);
+    }
+}
