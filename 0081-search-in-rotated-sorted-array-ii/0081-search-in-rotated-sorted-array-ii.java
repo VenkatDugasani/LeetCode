feb46e3a8cad729145980e11d93e029ced730982
VenkatDugasani <132843601+VenkatDugasani@users.noreply.github.com>
2025-11-05 21:34:52 -0800

    Time: 0 ms (100%), Space: 45.1 MB (5.26%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,63 +1,52 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        int length = nums.length;
-        if(length == 1){
-            if(target == nums[0]){
-                return true;
-            }
-            else{
-                return false;
+        int n = nums.length;
+        if(n == 0) return false;
+        if(n == 1){
+            if(nums[0] == target) return true;
+            else return false;
+        }
+        int  s = 0;
+        int e = n-1;
+        int r = 0;
+
+        if(nums[0] == nums[n-1])
+        {
+            for(int i = 0; i < n; i++)
+            {
+                if(nums[i] != nums[n-1])
+                {
+                    s = i;
+                    break;
+                }
             }
         }
-        int index = 0;
-        int start = index;
-        int end = length - 1;
-        while(index < length - 1){
-            if(nums[index] != nums[length - 1]){
-                break;
+        if(s == n-1 && target == nums[0]) return true;
+
+        if(target == nums[n-1]) return true;
+        if(target < nums[n-1])  r = 2;
+        if(target > nums[n-1]) r = 1;
+
+        while(s <= e)
+        {
+            int m = s + (e-s)/2;
+            if(target == nums[m]) return true;
+            if(nums[m] <= nums[n-1])
+            {
+                if(target < nums[m] || r == 1)
+                e = m-1;
+                else s = m + 1;
+            
             }
-            index++;
-        }
-        if(index == length - 1){
-            if(target == nums[length - 1]){
-                return true;
+            else
+            {
+                if(target > nums[m] || r == 2)
+                s = m + 1;
+                else e = m - 1;
             }
-            else{
-               return false;
-            }
+
         }
 
-        start = index;
-        end = length - 1;
-        String region = "green";
-        if(target == nums[length - 1]){
-            return true;
-        }
-        else if(target < nums[length -1 ]){
-            region = "orange";
-        }
-        while(start <= end){
-            int mid = start + (end - start) / 2;
-            if(nums[mid] == target){
-                return true;
-            }
-            if(nums[mid] <= nums[length - 1]){
-                if(target < nums[mid] || region == "green"){
-                    end = mid - 1;
-                }
-                else{
-                    start = mid + 1;
-                }
-            }
-            else{
-                if(target > nums[mid] || region == "orange"){
-                    start = mid + 1;
-                }
-                else{
-                    end = mid - 1;
-                }
-            }
-        }
         return false;
     }
 }
